4e6a9632a335c4d77da58ca54a2051223bbb61f6
eve <ewashadevsk@gmail.com>
2018-09-30 23:30:42 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+ let result = 0;
+  let i = 1;
+  while (Math.round(number / Math.pow(5, i))) {
+    result += Math.floor(number / Math.pow(5, i));
+    ++i;
+  }
+  return result;
 }
